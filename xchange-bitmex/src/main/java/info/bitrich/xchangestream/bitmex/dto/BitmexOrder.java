9dc9e8d201ba8cff498feb2e2012dfc2821cab77
Andrew Black <scijoker@gmail.com>
2020-02-09 21:19:19 +0200

    Updated BitmexOrder. Added Getter methods for all variables

--- xchange-bitmex/src/main/java/info/bitrich/xchangestream/bitmex/dto/BitmexOrder.java
+++ xchange-bitmex/src/main/java/info/bitrich/xchangestream/bitmex/dto/BitmexOrder.java
@@ -2,29 +2,14 @@ package info.bitrich.xchangestream.bitmex.dto;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import org.knowm.xchange.currency.CurrencyPair;
-import org.knowm.xchange.dto.Order;
-import org.knowm.xchange.dto.trade.LimitOrder;
-import org.knowm.xchange.dto.trade.MarketOrder;
 
 import java.math.BigDecimal;
 
 public class BitmexOrder extends BitmexMarketDataEvent {
-    public enum OrderStatus {
-        NEW,
-        PARTIALLYFILLED,
-        FILLED,
-        TBD,
-        CANCELED,
-        REJECTED,
-        UNKNOWN
-    }
-
     private String orderID;
     private String clOrdID;
     private String clOrdLinkID;
     private Long account;
-    //symbol here
     private String side;
     private BigDecimal simpleOrderQty;
     private BigDecimal orderQty;
@@ -52,7 +37,6 @@ public class BitmexOrder extends BitmexMarketDataEvent {
     private String multiLegReportingType;
     private String text;
     private String transactTime;
-    //symbol timestamp
 
     @JsonCreator
     public BitmexOrder(
@@ -97,87 +81,46 @@ public class BitmexOrder extends BitmexMarketDataEvent {
         this.symbol = symbol;
         this.side = side;
         this.simpleOrderQty = simpleOrderQty;
-        this.orderQty=orderQty;
-        this.price=price;
-        this.displayQty=displayQty;
-        this.stopPx=stopPx;
-        this.pegOffsetValue=pegOffsetValue;
-        this.pegPriceType=pegPriceType;
-        this.currency=currency;
+        this.orderQty = orderQty;
+        this.price = price;
+        this.displayQty = displayQty;
+        this.stopPx = stopPx;
+        this.pegOffsetValue = pegOffsetValue;
+        this.pegPriceType = pegPriceType;
+        this.currency = currency;
         this.settlCurrency = settlCurrency;
         this.ordType = ordType;
-        this.timeInForce=timeInForce;
-        this.execInst=execInst;
-        this.contingencyType=contingencyType;
-        this.exDestination=exDestination;
-        this.ordStatus=ordStatus;
-        this.triggered=triggered;
-        this.workingIndicator=workingIndicator;
-        this.ordRejReason=ordRejReason;
+        this.timeInForce = timeInForce;
+        this.execInst = execInst;
+        this.contingencyType = contingencyType;
+        this.exDestination = exDestination;
+        this.ordStatus = ordStatus;
+        this.triggered = triggered;
+        this.workingIndicator = workingIndicator;
+        this.ordRejReason = ordRejReason;
         this.simpleLeavesQty = simpleLeavesQty;
         this.leavesQty = leavesQty;
-        this.simpleCumQty=simpleCumQty;
-        this.cumQty=cumQty;
+        this.simpleCumQty = simpleCumQty;
+        this.cumQty = cumQty;
         this.avgPx = avgPx;
-        this.multiLegReportingType=multiLegReportingType;
-        this.text=text;
-        this.transactTime=transactTime;
-    }
-
-    private Order.Builder getOrderBuilderByType() {
-        Order.Builder builder;
-        if ("Limit".equalsIgnoreCase(ordType))
-            builder = new LimitOrder.Builder("Buy".equalsIgnoreCase(side) ? Order.OrderType.BID : Order.OrderType.ASK, new CurrencyPair(symbol.substring(0, 3), symbol.substring(3)));
-        else
-            builder = new MarketOrder.Builder("Buy".equalsIgnoreCase(side) ? Order.OrderType.BID : Order.OrderType.ASK, new CurrencyPair(symbol.substring(0, 3), symbol.substring(3)));
-        return builder;
-    }
-
-    private Order.OrderStatus getOrderStatus() {
-        Order.OrderStatus status= Order.OrderStatus.UNKNOWN;
-//        switch (ordStatus) {
-//            case NEW:
-//                status = Order.OrderStatus.NEW;
-//                break;
-//            case PARTIALLYFILLED:
-//                status = Order.OrderStatus.PARTIALLY_FILLED;
-//                break;
-//            case FILLED:
-//                status = Order.OrderStatus.FILLED;
-//                break;
-//            case TBD:
-//                status = Order.OrderStatus.PENDING_CANCEL;
-//                break;
-//            case CANCELED:
-//                status = Order.OrderStatus.CANCELED;
-//                break;
-//            case REJECTED:
-//                status = Order.OrderStatus.REJECTED;
-//                break;
-//            default:
-//                status = Order.OrderStatus.UNKNOWN;
-//                break;
-//
-//        }
-        return status;
-    }
-
-    public Order toOrder() {
-        return getOrderBuilderByType()
-                .id(orderID)
-                .orderType(Order.OrderType.EXIT_BID)
-                .averagePrice(avgPx)
-                .originalAmount(orderQty)
-                .cumulativeAmount(cumQty)
-                .orderStatus(getOrderStatus())
-                .build();
+        this.multiLegReportingType = multiLegReportingType;
+        this.text = text;
+        this.transactTime = transactTime;
     }
 
     public String getOrderID() {
         return orderID;
     }
 
-    public long getAccount() {
+    public String getClOrdID() {
+        return clOrdID;
+    }
+
+    public String getClOrdLinkID() {
+        return clOrdLinkID;
+    }
+
+    public Long getAccount() {
         return account;
     }
 
@@ -185,35 +128,107 @@ public class BitmexOrder extends BitmexMarketDataEvent {
         return side;
     }
 
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public BigDecimal getAvgPx() {
-        return avgPx;
-    }
-
-    public String getOrdType() {
-        return ordType;
-    }
-
-    public String getOrdStatus() {
-        return ordStatus;
-    }
-
-    public String getClOrdID() {
-        return clOrdID;
+    public BigDecimal getSimpleOrderQty() {
+        return simpleOrderQty;
     }
 
     public BigDecimal getOrderQty() {
         return orderQty;
     }
 
-    public BigDecimal getCumQty() {
-        return cumQty;
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public BigDecimal getDisplayQty() {
+        return displayQty;
+    }
+
+    public BigDecimal getStopPx() {
+        return stopPx;
+    }
+
+    public BigDecimal getPegOffsetValue() {
+        return pegOffsetValue;
+    }
+
+    public String getPegPriceType() {
+        return pegPriceType;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public String getSettlCurrency() {
+        return settlCurrency;
+    }
+
+    public String getOrdType() {
+        return ordType;
+    }
+
+    public String getTimeInForce() {
+        return timeInForce;
+    }
+
+    public String getExecInst() {
+        return execInst;
+    }
+
+    public String getContingencyType() {
+        return contingencyType;
+    }
+
+    public String getExDestination() {
+        return exDestination;
+    }
+
+    public String getOrdStatus() {
+        return ordStatus;
+    }
+
+    public String getTriggered() {
+        return triggered;
     }
 
     public boolean isWorkingIndicator() {
         return workingIndicator;
+    }
+
+    public String getOrdRejReason() {
+        return ordRejReason;
+    }
+
+    public BigDecimal getSimpleLeavesQty() {
+        return simpleLeavesQty;
+    }
+
+    public BigDecimal getLeavesQty() {
+        return leavesQty;
+    }
+
+    public BigDecimal getSimpleCumQty() {
+        return simpleCumQty;
+    }
+
+    public BigDecimal getCumQty() {
+        return cumQty;
+    }
+
+    public BigDecimal getAvgPx() {
+        return avgPx;
+    }
+
+    public String getMultiLegReportingType() {
+        return multiLegReportingType;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getTransactTime() {
+        return transactTime;
     }
 }
